a003fddd38a25823194228c98c1789c8d6bc5486
Headlessned <amanniyazov.dku@gmail.com>
2025-02-09 13:50:30 +0500

    второй коммит перед последующими шагами

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module exchangeBot
 
 go 1.23
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mitchellh/hashstructure v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tucnak/telebot v2.0.0+incompatible // indirect
+	gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gopkg.in/telebot.v4"
+	"log"
+	"os"
+	"time"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TG_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("Токен успешно загружен", token[:10]+"...")
+
+	bot, err := telebot.NewBot(telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Handle("/start", func(c telebot.Context) error {
+		return c.Send("Привет! Меня зовут Боб, я бот конвертер валют. " +
+			"Напиши /help, чтобы узнать как меня использовать ")
+	})
+
+	fmt.Println("Бот Боб запущен бро")
+	bot.Start()
 }
